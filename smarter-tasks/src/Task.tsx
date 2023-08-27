892c623b059b14b32b2fdbf6b5d78f1717c27c79
Kaiser Sakhi <kaisersakhi@gmail.com>
2023-08-27 21:43:08 +0530

    refactor task component

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -14,7 +14,7 @@ class Task extends React.Component<TaskProp> {
       <div className="TaskItem shadow-md border border-slate-100">
         <h2 className="text-base font-bold my-1">{`${this.props.title} (${this.props.dueDate})`} </h2>
         <p className="text-sm text-slate-500">
-           Description: {this.props.description}
+           {this.props.description}
         </p>
       </div>
     )
